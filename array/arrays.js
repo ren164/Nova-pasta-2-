5d01d967cc366d33e7df7cee8a363099400a525a
ren164 <sus84467@gmail.com>
2025-06-11 10:44:02 -0300

    s

---
+++ array/arrays.js
@@ -0,0 +1,22 @@
+let amigos = ["carlos", "Joana", "Marcos", "Barbara", "Luana"]
+
+console.log(amigos[3])
+
+for (let i = 0; i, 5 < amigos.length; i++) {
+    console.log(amigos[i])
+}
+console.log("adicionando na lista")
+amigos. push("Fernanda")
+console.log(amigos)
+
+console.log("remove o primeiro da lista")
+amigos.shift()
+console.log(amigos)
+
+console.log("remove o ultimo da lista")
+amigos.pop()
+console.log(amigos)
+
+console.log("adiciona no inicio da lista")
+amigos.unshift("Tati")
+console.log(amigos)
